449577f17c32573b6bd657bd0ef09a97fce389fe
Simon Godoy <theonlysimon@gmail.com>
2020-07-25 17:05:45 -0500

    Refactor notification test.

--- test/Messages/NotificationTest.cs
+++ test/Messages/NotificationTest.cs
@@ -42,43 +42,33 @@ namespace PlacetoPay.RedirectionTests.Messages
         {
             string data =
             "{  " +
-            "   \"status\":{  " +
-            "      \"status\":\"APPROVED\"," +
-            "      \"reason\":\"00\"," +
-            "      \"message\":\"Se ha aprobado su pago, puede imprimir el recibo o volver a la pagina del comercio\"," +
-            "      \"date\":\"2016-10-10T16:39:57-05:00\"" +
-            "   }," +
-            "   \"requestId\":83," +
-            "   \"reference\":\"TEST_20161010_213937\"," +
+            "   \"requestId\":1," +
+            "   \"reference\":\"TEST_20201010_213937\"," +
             "   \"signature\":\"8fb4beea130ab3e75a1de956bd0213892e0f6839\"" +
             "}";
 
             var notification = new Notification(data, "024h1IlD");
 
-            Assert.True(notification.IsValidNotification(), "Valid notification");
-            Assert.True(notification.IsApproved(), notification.GetStatus().StatusText);
-            Assert.False(notification.IsRejected(), notification.GetStatus().StatusText);
-            Assert.AreEqual(notification.RequestId, 83, "Same request identifier");
-            Assert.AreEqual(notification.Reference, "TEST_20161010_213937", "Same reference");
+            Assert.False(notification.IsApproved(), notification.GetStatus().StatusText);
+            Assert.AreEqual(notification.RequestId, 1);
+            Assert.AreEqual(notification.Reference, "TEST_20201010_213937");
         }
 
         [Test]
         public void Should_Parse_Correctly_The_Notification_With_Object_Instance()
         {
             Status status = new Status(new JObject {
-                { "status", "APPROVED" },
+                { "status", "REJECTED" },
                 { "reason", "00" },
-                { "message", "Se ha aprobado su pago, puede imprimir el recibo o volver a la pagina del comercio" },
-                { "date", "2016-10-10T16:39:57-05:00" },
+                { "message", "Pago rechazado" },
+                { "date", "2020-10-10T16:39:57-05:00" },
             });
 
-            var notification = new Notification(status, 83, "TEST_20161010_213937", "8fb4beea130ab3e75a1de956bd0213892e0f6839", "024h1IlD");
+            var notification = new Notification(status, 15, null, null, null);
 
-            Assert.True(notification.IsValidNotification(), "Valid notification");
-            Assert.True(notification.IsApproved(), notification.GetStatus().StatusText);
-            Assert.False(notification.IsRejected(), notification.GetStatus().StatusText);
-            Assert.AreEqual(notification.RequestId, 83, "Same request identifier");
-            Assert.AreEqual(notification.Reference, "TEST_20161010_213937", "Same reference");
+            Assert.True(notification.IsRejected(), notification.GetStatus().StatusText);
+            Assert.AreEqual("Pago rechazado", notification.GetStatus().Message);
+            Assert.IsNull(notification.Reference);
         }
     }
 }
